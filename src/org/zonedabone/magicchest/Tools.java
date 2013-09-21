f033649ceabf6649b16b93792e5ef8c37570d952
uvbeenzaned <uvbeenzaned2x@gmail.com>
2013-09-20 22:27:54 -0500

    Started rewriting sorting engine for Material.
    It's not pretty but...  I'm sure a lot of it is unneeded.  I'll probably
    end up switching back to a comparator, this is just a test using a
    TreeMap.

--- src/org/zonedabone/magicchest/Tools.java
+++ src/org/zonedabone/magicchest/Tools.java
@@ -1,13 +1,14 @@
 package org.zonedabone.magicchest;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+//import java.util.Collections;
+//import java.util.Comparator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
@@ -50,43 +51,80 @@ public class Tools {
 						stacks.add(is);
 					}
 				}
-				if(alpha)
-				{
+				if(alpha) {
 					SortedMap<String, ItemStack> alphastacks = new TreeMap<String, ItemStack>();
-					for(ItemStack item : stacks)
-					{
-						alphastacks.put(item.getType().toString(), item);
+					for(ItemStack item : stacks) {
+						boolean hasitem = false;
+						for(String k : alphastacks.keySet()) {
+							if(k.contains(item.getType().toString())) {
+								hasitem = true;
+							} else {
+								hasitem = false;
+							}
+						}
+						if(!hasitem && (!item.hasItemMeta() && (!item.getItemMeta().hasDisplayName() || !item.getItemMeta().hasLore()))) {
+							if(item.getItemMeta() != null && item.getItemMeta().hasDisplayName()) {
+								alphastacks.put(item.getItemMeta().getDisplayName() + ":" + String.valueOf(item.getDurability()), item);
+								Bukkit.broadcastMessage(item.getItemMeta().getDisplayName() + ":" + String.valueOf(item.getDurability()));
+							} else {
+								alphastacks.put(item.getType().toString() + ":" + String.valueOf(item.getDurability()), item);
+								Bukkit.broadcastMessage(item.getType().toString() + ":" + String.valueOf(item.getDurability()));
+							}
+						} else {
+							int icnt = 0;
+							int lorecnt = 0;
+							for(String n : alphastacks.keySet()) {
+								if(alphastacks.get(n).getItemMeta().hasDisplayName() && n.contains(item.getItemMeta().getDisplayName())) {
+									icnt++;
+								}
+								if(alphastacks.get(n).getItemMeta().hasLore()) {
+									lorecnt++;
+								}
+							}
+							if(!item.getItemMeta().hasLore() && item.getItemMeta().hasDisplayName()) {
+								alphastacks.put(item.getItemMeta().getDisplayName() + ":" + String.valueOf(item.getDurability()) + "," + String.valueOf(lorecnt) + "," + String.valueOf(icnt), item);
+								Bukkit.broadcastMessage(item.getItemMeta().getDisplayName() + ":" + String.valueOf(item.getDurability()) + "," + String.valueOf(lorecnt) + "," + String.valueOf(icnt));
+							} else {
+								if(item.getItemMeta().hasLore() && !item.getItemMeta().hasDisplayName()) {
+									alphastacks.put(item.getType().toString() + ":" + String.valueOf(item.getDurability()) + "," + String.valueOf(lorecnt) + "," + String.valueOf(icnt), item);
+									Bukkit.broadcastMessage(item.getType().toString() + ":" + String.valueOf(item.getDurability()) + "," + String.valueOf(lorecnt) + "," + String.valueOf(icnt));
+								} else {
+									if(!item.getItemMeta().hasLore() && !item.getItemMeta().hasDisplayName())
+										alphastacks.put(item.getType().toString() + ":" + String.valueOf(item.getDurability()) + "," + String.valueOf(icnt), item);
+									Bukkit.broadcastMessage(item.getType().toString() + ":" + String.valueOf(item.getDurability()) + "," + String.valueOf(icnt));
+								}
+							}
+						}
 					}
 					stacks.clear();
-					for(Entry<String, ItemStack> item : alphastacks.entrySet())
-					{
+					for(Entry<String, ItemStack> item : alphastacks.entrySet()) {
 						stacks.add(item.getValue());
 					}
 				}
-				else
-				{
-					Collections.sort(stacks, new Comparator<ItemStack>() {
-
-						@Override
-						public int compare(ItemStack o1, ItemStack o2) {
-							if (o1.getTypeId() > o2.getTypeId()) {
-								return 1;
-							} else if (o1.getTypeId() < o2.getTypeId()) {
-								return -1;
-							} else if (o1.getData() != null && o2.getData() != null && o1.getData().getData() > o2.getData().getData()) {
-								return 1;
-							} else if (o1.getData() != null && o2.getData() != null && o1.getData().getData() < o2.getData().getData()) {
-								return -1;
-							} else if (o1.getAmount() > o2.getAmount()) {
-								return -1;
-							} else if (o1.getAmount() < o2.getAmount()) {
-								return 1;
-							} else {
-								return 0;
-							}
-						}
-					});
-				}
+//				else
+//				{
+//					Collections.sort(stacks, new Comparator<ItemStack>() {
+//
+//						@Override
+//						public int compare(ItemStack o1, ItemStack o2) {
+//							if (o1.getTypeId() > o2.getTypeId()) {
+//								return 1;
+//							} else if (o1.getTypeId() < o2.getTypeId()) {
+//								return -1;
+//							} else if (o1.getData() != null && o2.getData() != null && o1.getData().getData() > o2.getData().getData()) {
+//								return 1;
+//							} else if (o1.getData() != null && o2.getData() != null && o1.getData().getData() < o2.getData().getData()) {
+//								return -1;
+//							} else if (o1.getAmount() > o2.getAmount()) {
+//								return -1;
+//							} else if (o1.getAmount() < o2.getAmount()) {
+//								return 1;
+//							} else {
+//								return 0;
+//							}
+//						}
+//					});
+//				}
 				e.getInventory().clear();
 				e.getInventory().setContents(stacks.toArray(new ItemStack[0]));
 			}
